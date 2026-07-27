9e14775ec37fca904996fc4d748c22d226bbebc0
Sophia Liu <sl3632@cornell.edu>
2026-07-27 19:12:16 -0400

    always collapse video

--- app/mediator/page.tsx
+++ app/mediator/page.tsx
@@ -219,7 +219,6 @@ export default function Home() {
     if (authReady && !localStorage.getItem('tourLoaded')) {
       localStorage.setItem('tourLoaded', '1')
       startTour()
-      setShowTutorial(true);
     }
   }, [authReady])
 
