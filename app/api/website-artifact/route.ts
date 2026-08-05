87933b06f87c1c9d83eae8790d7601df1a4c51f8
Sophia Liu <sl3632@cornell.edu>
2026-08-05 03:03:08 -0400

    add api base

--- app/api/website-artifact/route.ts
+++ app/api/website-artifact/route.ts
@@ -1,5 +1,7 @@
+import { API_BASE } from '../../lib/config'
+
 export async function POST(request: Request) {
-  const createExperimentUrl = new URL('/api/create-experiment', request.url)
+  const createExperimentUrl = new URL(`${API_BASE}/api/create-experiment`, request.url)
 
   const res = await fetch(createExperimentUrl, {
     method: 'POST',
