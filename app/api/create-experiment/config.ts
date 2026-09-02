031cf0d30ec7d50cc936209a9d92bcce23a1b42c
Sophia Liu <sl3632@cornell.edu>
2026-09-02 15:42:51 -0400

    Fix: Change to -assistant from dev
    Problem: Wrong firebase deployment (dev)
    Solution: Changed to right firebase deployment (-assistnat)

--- app/api/create-experiment/config.ts
+++ app/api/create-experiment/config.ts
@@ -5,18 +5,18 @@ export const SEED = 123
 const LOCAL = process.env.NODE_ENV === 'development'
 
 export const BASE_URL = LOCAL
- ? 'http://127.0.0.1:5001/convoarenadev/us-central1/api/v1'
- : 'https://us-central1-convoarenadev.cloudfunctions.net/api/v1'
+ ? 'http://127.0.0.1:5001/convoarena-assistant/us-central1/api/v1'
+ : 'https://us-central1-convoarena-assistant.cloudfunctions.net/api/v1'
 
 
 export const CREATE_PARTICIPANT_URL = LOCAL
- ? 'http://127.0.0.1:5001/convoarenadev/us-central1/createParticipant'
- : 'https://us-central1-convoarenadev.cloudfunctions.net/createParticipant'
+ ? 'http://127.0.0.1:5001/convoarena-assistant/us-central1/createParticipant'
+ : 'https://us-central1-convoarena-assistant.cloudfunctions.net/createParticipant'
 
 
 export const FRONTEND_BASE = LOCAL
  ? 'https://localhost:4201'
- : 'https://convoarenadev.web.app/'
+ : 'https://convoarena-assistant.web.app/'
 
 export const API_KEY = process.env.DL_API_KEY ?? ''
 
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -7,14 +7,14 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfyqzBXPAdeaWrRzHVaUzjHl_0HumzhCo",
-  authDomain: "convoarenadev.firebaseapp.com",
-  databaseURL: "https://convoarenadev-default-rtdb.firebaseio.com",
-  projectId: "convoarenadev",
-  storageBucket: "convoarenadev.firebasestorage.app",
-  messagingSenderId: "634368617031",
-  appId: "1:634368617031:web:341548305229f049845f62",
-  measurementId: "G-WJ3VZHHEF4"
+  apiKey: "AIzaSyBRkYfri2Uy3qdy3emGHHKz6qpDU8fO_1w",
+  authDomain: "convoarena-assistant.firebaseapp.com",
+  databaseURL: "https://convoarena-assistant-default-rtdb.firebaseio.com",
+  projectId: "convoarena-assistant",
+  storageBucket: "convoarena-assistant.firebasestorage.app",
+  messagingSenderId: "173430569358",
+  appId: "1:173430569358:web:c110ecc5b481be862fccdf",
+  measurementId: "G-KSLD8SMVL6"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
