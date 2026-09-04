5f648d32786c72aaa45d6884cb6fb2a661cc97ce
Sophia Liu <sl3632@cornell.edu>
2026-09-04 16:32:03 -0400

    Fix: Add back config vars
    Problem: They were deleted on accident
    Solution: Add them back
    Testing: `npm run build` works

--- app/api/create-experiment/config.ts
+++ app/api/create-experiment/config.ts
@@ -24,3 +24,19 @@ export const CREATE_PARTICIPANT_URL = LOCAL
 export const FRONTEND_BASE = LOCAL
 ? 'https://localhost:4201'
 : 'https://convoarenadev.web.app/'
+
+
+export const API_KEY = process.env.DL_API_KEY ?? ''
+
+export const PROJECT_ROOT = process.cwd()
+
+export const MEDIATOR_DEFAULT = path.join(PROJECT_ROOT, 'public', 'templates', 'defaults', 'mediator.yaml')
+export const ASSISTANT_DEFAULT = path.join(PROJECT_ROOT, 'public', 'templates', 'defaults', 'assistant.yaml')
+export const EXPERIMENT_DEFAULT = path.join(PROJECT_ROOT, 'public', 'templates', 'defaults', 'experiment.yaml')
+export const COMPETITION_MEDIATOR = path.join(PROJECT_ROOT, 'public', 'templates', 'competition', 'mediator.yaml')
+
+export const STAGE_R1 = 'chat-round-1'
+export const PRE_SURVEY_STAGE_ID = "pre-survey"
+export const POST_SURVEY_STAGE_ID = "post-survey"
+
+export const COMPLETION_CODE = ''
