68049a670acc642f41751aaec0d51cfe184fc6c5
Christina <christinab2314@gmail.com>
2026-09-04 15:36:56 -0400

    configs

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,18 +4,20 @@ import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRkYfri2Uy3qdy3emGHHKz6qpDU8fO_1w",
-  authDomain: "convoarena-assistant.firebaseapp.com",
-  databaseURL: "https://convoarena-assistant-default-rtdb.firebaseio.com",
-  projectId: "convoarena-assistant",
-  storageBucket: "convoarena-assistant.firebasestorage.app",
-  messagingSenderId: "173430569358",
-  appId: "1:173430569358:web:c110ecc5b481be862fccdf",
-  measurementId: "G-KSLD8SMVL6"
+ apiKey: "AIzaSyCfyqzBXPAdeaWrRzHVaUzjHl_0HumzhCo",
+ authDomain: "convoarenadev.firebaseapp.com",
+ databaseURL: "https://convoarenadev-default-rtdb.firebaseio.com",
+ projectId: "convoarenadev",
+ storageBucket: "convoarenadev.firebasestorage.app",
+ messagingSenderId: "634368617031",
+ appId: "1:634368617031:web:341548305229f049845f62",
+ measurementId: "G-WJ3VZHHEF4"
 };
+
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
 export const auth = getAuth(app)
